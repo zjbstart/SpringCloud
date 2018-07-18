ad3639ec485afae3a15c2c98dc3dcb860028e143
zjbstart4723 <729774723@qq.com>
2018-07-18 21:52:44 +0800

    搭建8001服务提供者的工程 microservice-provider-dept-8001

--- microservicecloud-common/src/main/java/com/atguigu/springcloud/entities/Dept.java
+++ microservicecloud-common/src/main/java/com/atguigu/springcloud/entities/Dept.java
@@ -1,5 +1,53 @@
 package com.atguigu.springcloud.entities;
 
-public class Dept {
+import java.io.Serializable;
 
-}
+@SuppressWarnings("serial")
+public class Dept implements Serializable {
+
+	private Long deptNo;
+	private String dname;
+	private String db_source;
+	
+	public Dept() {
+		
+	}
+
+	public Dept(Long deptNo, String dname, String db_source) {
+		super();
+		this.deptNo = deptNo;
+		this.dname = dname;
+		this.db_source = db_source;
+	}
+
+	public Long getDeptNo() {
+		return deptNo;
+	}
+
+	public void setDeptNo(Long deptNo) {
+		this.deptNo = deptNo;
+	}
+
+	public String getDname() {
+		return dname;
+	}
+
+	public void setDname(String dname) {
+		this.dname = dname;
+	}
+
+	public String getDb_source() {
+		return db_source;
+	}
+
+	public void setDb_source(String db_source) {
+		this.db_source = db_source;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("hello ");
+	}
+	
+	
+	
+} 
